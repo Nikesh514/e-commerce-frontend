a10577ef62bbeb6bb8bdf62b0ed118862e71c142
Nikesh Mainali <nikeshmainali10@gmail.com>
2025-07-17 14:06:54 +0545

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Greet from './components/greet'
+import Second from './components/second'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Shop Kart</h1>
+      <Greet />
+      <Second />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/second.tsx
@@ -0,0 +1,13 @@
+const Second = () => {
+    return (
+        <div>
+            <div>
+                <p>
+                    Welcome to the Second page
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Second
