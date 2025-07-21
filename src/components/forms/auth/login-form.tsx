3c6b45cc83e61b51f49b7deed4212fe892abcecc
Nikesh Mainali <nikeshmainali10@gmail.com>
2025-07-21 14:56:12 +0545

    added login page

--- src/components/forms/auth/login-form.tsx
+++ src/components/forms/auth/login-form.tsx
@@ -4,8 +4,8 @@
 const LoginForm = () => {
   return (
     <div>
-        <form>
-            <div>
+        <form className="flex flex-col gap-10">
+            <div className="flex flex-col gap-4">
                 {/* email */}
                 <div className="flex flex-col gap-1">
                     <label 
@@ -21,9 +21,12 @@ const LoginForm = () => {
                 </div>
 
                 {/* password */}
-                <div>
-                    <label htmlFor={"password"}>Password</label>
+                <div className="flex flex-col gap-1">
+                    <label 
+                        className="text-xl font-semibold text-gray-700"
+                        htmlFor={"password"}>Password</label>
                     <input
+                    className="border border-grey-300 rounded-md p-2 placeholder:text-sm text-lg focus:outline-blue-500"
                     placeholder="enter your password"
                     type={'password'}
                     name="password"
@@ -32,7 +35,9 @@ const LoginForm = () => {
                 </div>
             </div>
 
-            <button type="submit">Login</button>
+            <button 
+            className="bg-blue-500 w-full p-2 rounded-md text-white test-lg font-semibold hover:bg-blue-600 cursor-pointer transition-all duration-300"
+            type="submit">Login</button>
         </form>
     </div>
   )
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -3,8 +3,8 @@ import LoginForm from "../../components/forms/auth/login-form"
 const Login = () => {
 
     return (
-        <main className="border h-full flex flex-col items-center justify-center gap-4 p-4">
-            <div className="border border-blue-500 w-full px-4 py2 rounded-md">
+        <main className="border h-full flex flex-col items-center justify-center  p-3">
+            <div className="border border-blue-500 w-[min(100%, 500px)] px-4 py-6 rounded-md">
                 {/* page heading */}
                 <h1 className="test-3xl font-bold text-center text-gray-800 mb-6">Login</h1>
                 
@@ -13,6 +13,12 @@ const Login = () => {
                 <div>
                     <LoginForm/>
                 </div>
+
+                <div className="text-center mt-4">
+                    <p className="text-gray-600">Forgot your password? <span className="text-blue-500 font-semibold">Reset Password</span></p>
+                    <p>Does not have an account? <span className=" text-blue-500 font-semibold ">Sign Up</span></p>
+                </div>
+
             </div>
         </main>
     )
