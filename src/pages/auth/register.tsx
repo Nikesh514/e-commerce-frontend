49d98002e79e24636bd392d2ce4ccd024b3e3274
Nikesh Mainali <nikeshmainali10@gmail.com>
2025-07-23 23:01:00 +0545

    minor changes

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,32 +1,20 @@
-import './login.css'
+import RegisterForm from "../../components/forms/auth/register-form"
 
-const Register  = () => {
+const Register = () => {
+
     return (
-        <main>
-            <h1>
-                Login
-            </h1>
-            {/* from */}
-            <div>
-                { /* email */}
-                <div className="input-wrapper">
-                    <label htmlFor="email">Email</label>
-                    <input
-                    name = 'email' 
-                    id = "email"
-                    placeholder = "johndoe@gmai.com"
-                    type = "email"
-                    />
+        <main className="border h-full flex flex-col items-center justify-center p-3">
+            <div className="border border-blue-500 w-[min(100%, 500px)] px-4 py-6 rounded-md">
+                {/* page heading */}
+                <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">Register</h1>
+
+                {/* register form */}
+                <div>
+                    <RegisterForm/>
                 </div>
-                {/* password */}
-                <div className="input-wrapper">
-                <label htmlFor="email">Password</label>
-                    <input
-                    name = 'password' 
-                    id = "email"
-                    placeholder = "enter your password"
-                    type = "password"
-                    />
+
+                <div className="text-center mt-4">
+                    <p className="text-gray-600">Already have an account? <span className="text-blue-500 font-semibold">Login</span></p>
                 </div>
             </div>
         </main>
